2d19791ce9bb49fb5b6b125391ef4895a1853bb3
pande <pande@192.168.2.8>
2016-06-27 20:04:51 +0530

    removed stdlib

--- test_pro_C/src/test_pro_C.c
+++ test_pro_C/src/test_pro_C.c
@@ -9,9 +9,9 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) {
+	printf("Hello");
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+	return 1;
 }
